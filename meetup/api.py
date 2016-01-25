0f6ca31e07b666c0b0d3bfd5799fa4a7ce503800
Pat Ferate <pferate+github@gmail.com>
2016-01-25 09:45:48 -0800

    Moving module config definitions to __init__.py

--- meetup/api.py
+++ meetup/api.py
@@ -3,17 +3,8 @@ import os
 import six
 import slumber
 
+from meetup import API_DEFAULT_URL, API_KEY_ENV_NAME, API_SERVICE_FILES
 from meetup.exceptions import ApiKeyException, ApiMethodNotDefined
-
-
-API_DEFAULT_URL = 'http://api.meetup.com/'
-API_KEY_ENV_NAME = 'MEETUP_API_KEY'
-CONFIG_DIR = os.path.join(os.path.dirname(__file__), 'config')
-API_SERVICE_FILES = {
-    'v1': os.path.join(CONFIG_DIR, 'meetup_v1_services.json'),
-    'v2': os.path.join(CONFIG_DIR, 'meetup_v2_services.json'),
-    'v3': os.path.join(CONFIG_DIR, 'meetup_v3_services.json'),
-}
 
 
 class Client(object):
