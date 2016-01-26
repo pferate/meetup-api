83e2adf7fe8d35fbe01cf30502e847efbadb93ea
Pat Ferate <pferate+github@gmail.com>
2016-01-26 09:36:53 -0800

    Changing name of API Specification directory

--- meetup/__init__.py
+++ meetup/__init__.py
@@ -4,9 +4,9 @@ import six
 
 API_DEFAULT_URL = 'http://api.meetup.com/'
 API_KEY_ENV_NAME = 'MEETUP_API_KEY'
-CONFIG_DIR = os.path.join(os.path.dirname(__file__), 'config')
+API_SPEC_DIR = os.path.join(os.path.dirname(__file__), 'api_specification')
 API_SERVICE_FILES = [
-    ('v1', os.path.join(CONFIG_DIR, 'meetup_v1_services.json')),
-    ('v2', os.path.join(CONFIG_DIR, 'meetup_v2_services.json')),
-    ('v3', os.path.join(CONFIG_DIR, 'meetup_v3_services.json')),
+    ('v1', os.path.join(API_SPEC_DIR, 'meetup_v1_services.json')),
+    ('v2', os.path.join(API_SPEC_DIR, 'meetup_v2_services.json')),
+    ('v3', os.path.join(API_SPEC_DIR, 'meetup_v3_services.json')),
 ]
