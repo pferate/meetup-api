de5162588737fb4215728f2c821a29c6d649a263
Pat Ferate <pferate+github@gmail.com>
2016-01-26 12:07:52 -0800

    Moving API variable definitions to the API file

--- meetup/api.py
+++ meetup/api.py
@@ -5,8 +5,17 @@ import requests
 import six
 from time import sleep
 
-from meetup import API_DEFAULT_URL, API_KEY_ENV_NAME, API_SERVICE_FILES
 from meetup import exceptions
+
+
+API_DEFAULT_URL = 'http://api.meetup.com/'
+API_KEY_ENV_NAME = 'MEETUP_API_KEY'
+API_SPEC_DIR = os.path.join(os.path.dirname(__file__), 'api_specification')
+API_SERVICE_FILES = [
+    ('v1', os.path.join(API_SPEC_DIR, 'meetup_v1_services.json')),
+    ('v2', os.path.join(API_SPEC_DIR, 'meetup_v2_services.json')),
+    ('v3', os.path.join(API_SPEC_DIR, 'meetup_v3_services.json')),
+]
 
 
 class MeetupObject(object):
