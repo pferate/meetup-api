a0f1507546c31f24f4be2763cbafa6368d300836
Pat Ferate <pferate+github@gmail.com>
2016-01-30 21:05:19 -0800

    Attempting to wait prior to getting a 429 error

--- meetup/api.py
+++ meetup/api.py
@@ -136,10 +136,12 @@ class Client(object):
         if response.status_code == 404:
             raise exceptions.HttpNotFoundError
         if response.status_code == 429:
-            if self.overlimit_wait:
-                sleep(1 + int(self.rate_limit.reset))
-                self._call(service_name, parameters)
-            else:
-                raise exceptions.HttpTooManyRequests
+            raise exceptions.HttpTooManyRequests
+
+        # If we have two or less remaining calls in the period, wait (if the wait flag is set).
+        # I tried only waiting after a 429 error, and ended getting locked out doing parallel testing.
+        if self.rate_limit.remaining <= 2 and self.overlimit_wait:
+            sleep(1 + int(self.rate_limit.reset))
+            self._call(service_name, parameters)
 
         return MeetupObject(response.json())
