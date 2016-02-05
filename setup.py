f108bd11b4c5698c3ddc6043f6daa5089d58e65c
Pat Ferate <pferate+github@gmail.com>
2016-02-05 10:37:28 -0800

    Updating PKG-INFO values

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
+import io
 from setuptools import setup, find_packages
 
 from meetup import __version__
 
 package_data = {
     'api_specification': ['api_specification/*.json']}
+
+
+readme_file = io.open('README.rst', encoding='utf-8')
+
+with readme_file:
+    long_description = readme_file.read()
 
 setup(
     name='meetup-api',
@@ -16,7 +23,7 @@ setup(
     author='Pat Ferate',
     author_email='',
     description='Python API for Meetup',
-    long_description='Python API for Meetup',
+    long_description=long_description,
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
@@ -26,7 +33,6 @@ setup(
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Topic :: Internet :: WWW/HTTP',
